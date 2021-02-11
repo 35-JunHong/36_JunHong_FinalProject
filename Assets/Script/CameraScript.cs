3403d9fe4115fc59ae0296ae4fb11ce9cb6551f7
JunHong30 <neojunhong2003@gmail.com>
2021-02-11 16:01:53 +0800

    updated(camera)

---
+++ Assets/Script/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject playerGo;
+    Vector3 CameraOffSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraOffSet = new Vector3(0 , 2, -2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerGo.transform.position + CameraOffSet;
+    }
+}
